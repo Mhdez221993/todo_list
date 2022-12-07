1fdf7759a10de569f986f787fca60d7a95f447a2
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-07 18:29:22 +0100

    Access list, input and form fro index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+// import { v4 as uuid4 } from 'uuid';
 
+// console.log(uuid4());
+
+const list = document.querySelector<HTMLUListElement>('#list')
+const form = document.getElementById('task-form') as HTMLFormElement | null
+const input = document.querySelector<HTMLInputElement>('#task-input')
