70e5c98dea75f4d8df352ac2b70710cdaca35ab3
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-07 18:42:56 +0100

    Add event listener and create task structure

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
-// import { v4 as uuid4 } from 'uuid';
-
-// console.log(uuid4());
+import { v4 as uuid4 } from 'uuid'
 
 const list = document.querySelector<HTMLUListElement>('#list')
 const form = document.getElementById('task-form') as HTMLFormElement | null
 const input = document.querySelector<HTMLInputElement>('#task-input')
+
+form?.addEventListener('submit', (e) => {
+  if (input?.value == '' || input?.value == null) return
+
+  const task = {
+    id: uuid4(),
+    tittle: input.value,
+    completed: false,
+    createdAt: new Date(),
+  }
+})
